9ab449344d27ac3248591dce28e388980bfc70c5
SHREEVALLABH-PC\Shreevallabh <shreevallabh29@gmail.com>
2020-09-26 16:27:13 -0400

    Added colliders and testing scripts

--- Assets/Scripts/Pathfinding.cs
+++ Assets/Scripts/Pathfinding.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Pathfinding : MonoBehaviour
 {
+    public GameObject destination;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(destination.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       transform.Translate(-Vector3.left * Time.deltaTime);
     }
 }
