acae9c22a3bf5573c92c8b3883dc8bddca76544a
SHREEVALLABH-PC\Shreevallabh <shreevallabh29@gmail.com>
2020-09-27 22:05:20 -0400

    Working pathfinding but optimization WIP

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    public static List<Vector3> frontierLocations = new List<Vector3>();
+    public static List<Vector3> checkedLocations = new List<Vector3>();
+
+    public static List<Vector3> GetNeightbours(Vector3 loc, bool getNonFrontOnly)
+    {
+        Vector3 top = new Vector3(loc.x, loc.y - 1, 0);
+        Vector3 topRight = new Vector3(loc.x + 1, loc.y - 1, 0);
+        Vector3 right = new Vector3(loc.x + 1, loc.y, 0);
+        Vector3 bottomRight = new Vector3(loc.x + 1, loc.y + 1, 0);
+        Vector3 bottom = new Vector3(loc.x, loc.y + 1, 0);
+        Vector3 bottomLeft = new Vector3(loc.x - 1, loc.y + 1, 0);
+        Vector3 left = new Vector3(loc.x - 1, loc.y, 0);
+        Vector3 topLeft = new Vector3(loc.x - 1, loc.y - 1, 0);
+
+        //Debug.Log(top);
+        //Debug.Log(topRight);
+        //Debug.Log(right);
+        //Debug.Log(bottomRight);
+        //Debug.Log(bottom);
+        //Debug.Log(bottomLeft);
+        //Debug.Log(left);
+        //Debug.Log(topLeft);
+
+        List<Vector3> neighbours = new List<Vector3>();
+        neighbours.Add(top);
+        neighbours.Add(topRight);
+        neighbours.Add(right);
+        neighbours.Add(bottomRight);
+        neighbours.Add(bottom);
+        neighbours.Add(bottomLeft);
+        neighbours.Add(left);
+        neighbours.Add(topLeft);
+
+        if(getNonFrontOnly)
+        {
+            int temp = 0;
+            for( int i = 0; i < 8; i++)
+            {
+                if(frontierLocations.Contains(neighbours[temp]))
+                {
+                    Debug.Log("Found duplicate");
+                    neighbours.RemoveAt(0);
+                }
+                else
+                {
+                    temp++;
+                }
+            }
+        }
+
+        return neighbours;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+ 
+}
--- Assets/Scripts/Pathfinding.cs
+++ Assets/Scripts/Pathfinding.cs
@@ -4,17 +4,60 @@ using UnityEngine;
 
 public class Pathfinding : MonoBehaviour
 {
+    public GameObject source;
     public GameObject destination;
+    public GameObject path;
 
-    // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(destination.transform.position);
+        List<Vector3> initialNeighbours = Controller.GetNeightbours(source.transform.position, false);
+
+        Controller.frontierLocations.AddRange(initialNeighbours);
+
+        // Adding known locations to checked list which can never be destination e.g. location of source or walls
+        Controller.checkedLocations.Add(source.transform.position);
+        Controller.checkedLocations.Add(destination.transform.position);
+
+        while (Controller.frontierLocations.Count > 0)
+        {
+
+            // Did we reach destination?
+            if(Controller.frontierLocations[0] == destination.transform.position)
+            {
+                Debug.Log("Reached destination");
+                Debug.Log("frontierLocations list" + Controller.frontierLocations);
+                Debug.Log("checkedLocations list" + Controller.checkedLocations);
+                for( int i = 0; i < Controller.checkedLocations.Count; i++)
+                {
+                    Instantiate(path, Controller.checkedLocations[i], Quaternion.identity);
+                }
+                for (int i = 0; i < Controller.frontierLocations.Count; i++)
+                {
+                    Instantiate(source, Controller.frontierLocations[i], Quaternion.identity);
+                }
+                break;
+            }
+
+
+            // Have we checked current frontier location already?
+            if(Controller.checkedLocations.Contains(Controller.frontierLocations[0]))
+            {
+                Controller.frontierLocations.RemoveAt(0);
+            }
+            // if not, then add to checked list
+            else
+            {
+                Controller.checkedLocations.Add(Controller.frontierLocations[0]);
+                Controller.frontierLocations.AddRange(Controller.GetNeightbours(Controller.frontierLocations[0], true));
+                Controller.frontierLocations.RemoveAt(0);
+            }
+        }
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-       transform.Translate(-Vector3.left * Time.deltaTime);
+        //transform.Translate(-Vector3.left * Time.deltaTime);
+
     }
 }
